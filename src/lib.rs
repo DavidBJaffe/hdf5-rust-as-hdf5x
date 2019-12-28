b883e3a34cfa20ada952629252f5bed26485a4a6
Patrick Marks <pmarks@gmail.com>
2019-12-27 20:18:40 -0800

    wip on attribute support

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,8 @@ mod export {
         hl::extents::{Extent, Extents, SimpleExtents},
         hl::selection::{Hyperslab, Selection, SliceOrIndex},
         hl::{
-            Container, Conversion, Dataset, DatasetBuilder, Dataspace, Datatype, File, FileBuilder,
+            Container, Conversion, Dataset, Attribute, AttributeBuilder, 
+            DatasetBuilder, Dataspace, Datatype, File, FileBuilder,
             Group, Location, Object, PropertyList, Reader, Writer,
         },
     };
